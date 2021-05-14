36e12d7d56ce82d0e16c3a053a611753833aa3b5
phuongtung1 <phuongtung1231992@yahoo.com>
2021-05-14 11:01:15 +0800

    update

--- src/components/main/Landing.js
+++ src/components/main/Landing.js
@@ -3,7 +3,7 @@ import "./Landing.css";
 import { Auth } from "aws-amplify";
 import { onAuthUIStateChange } from "@aws-amplify/ui-components";
 import { AuthContext } from "../../Contexts";
-import { AmplifyAuthenticator, AmplifySignIn } from "@aws-amplify/ui-react";
+import { AmplifyAuthenticator, AmplifySignIn, AmplifySignUp } from "@aws-amplify/ui-react";
 import { Space } from "antd";
 import helpJSON from "../../assets/help/help_text_json";
 
@@ -23,41 +23,41 @@ function NotAuthenticated() {
     useEffect(() => onAuthUIStateChange(authUser));
 
     return (
-        // <AmplifyAuthenticator usernameAlias="email">
-        //   <AmplifySignUp
-        //     slot="sign-up"
-        //     usernameAlias="email"s
-        //     headerText="Create a Mobius-exo Account"
-        //     formFields={[
-        //       {
-        //         type: "email",
-        //         label: "E-mail",
-        //         placeholder: "email@email.mail",
-        //         required: true
-        //       },
-        //       {
-        //         type: "password",
-        //         label: "Password",
-        //         placeholder: "********",
-        //         required: true
-        //       },
-        //       {
-        //         type: "nickname",
-        //         label: "How should we address you?",
-        //         placeholder: "nickname",
-        //         required: true
-        //       }
-        //     ]}
-        //   />
-        //   <AmplifySignIn
-        //     slot="sign-in"
-        //     usernameAlias="email"
-        //     headerText="Get started!"
-        //   />
-        // </AmplifyAuthenticator>
         <AmplifyAuthenticator usernameAlias="email">
-            <AmplifySignIn slot="sign-in" usernameAlias="email" headerText="Get started!" hideSignUp />
+          <AmplifySignUp
+            slot="sign-up"
+            usernameAlias="email"s
+            headerText="Create a Mobius-exo Account"
+            formFields={[
+              {
+                type: "email",
+                label: "E-mail",
+                placeholder: "email@email.mail",
+                required: true
+              },
+              {
+                type: "password",
+                label: "Password",
+                placeholder: "********",
+                required: true
+              },
+              {
+                type: "nickname",
+                label: "How should we address you?",
+                placeholder: "nickname",
+                required: true
+              }
+            ]}
+          />
+          <AmplifySignIn
+            slot="sign-in"
+            usernameAlias="email"
+            headerText="Get started!"
+          />
         </AmplifyAuthenticator>
+        // <AmplifyAuthenticator usernameAlias="email">
+        //     <AmplifySignIn slot="sign-in" usernameAlias="email" headerText="Get started!" hideSignUp />
+        // </AmplifyAuthenticator>
     );
 }
 
