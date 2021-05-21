a3757b0d10c501dde2d76fbe72f495d229537815
phuongtung1 <phuongtung1231992@yahoo.com>
2021-05-21 15:10:40 +0800

    update

--- gen_eval_process_backend/src/decorators/porttypes-aware.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { InputType } from '../model/port';
-
-export function PortTypesAware(constructor: Function) {
-    constructor.prototype.PortTypes = InputType;
-
-    // array form
-    const keys = Object.keys(InputType);
-    constructor.prototype.PortTypesArr = keys.slice(keys.length / 2);
-}
